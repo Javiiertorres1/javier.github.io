4a8a1ebf05304a9d489471cf890cbe3308dc708c
Javiiertorres1 <javieroscartorres1@gmail.com>
2024-11-19 15:40:04 -0300

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function changeLanguage(language){
+  // oculta todos los elementos con data-lang
+  const elements = document.querySelectorAll('[data-lang]');
+  elements.forEach(element => {
+    if (element.getAttribute('data-lang') === language {
+        element.style.display = '';
+    } else {
+      element.style.display = 'none';
+    }
+});
+}
+  
